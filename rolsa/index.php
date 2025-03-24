6c1aabf369390f24d15b9bd551406746e0f96e02
MohabHelmi <144679684+MohabHelmi@users.noreply.github.com>
2025-03-24 14:00:32 +0000

    Add files via upload

---
+++ rolsa/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!--Links to other files-->
+    <link rel="stylesheet" href="style.css">
+    <link rel="related" href="bookings.php">
+    <link rel="related" href="order.php">
+    <link rel="related" href="products.php">
+    <link rel="related" href="cfp.php">
+    <link rel="related" href="login.php">
+
+    <!--Navigation bar-->
+<ul>
+    <li><button href="index.php" class="nav_btn">Home</a></li>
+    <li><button href="bookings.php" class="nav_btn">Bookings</a></li>
+    <li><button href="order.php" class="nav_btn">Order</a></li>
+    <li><button href="products.php" class="nav_btn">Products</a></li>
+    <li><button href="cfp.php" class="nav_btn">Bookings</a></li>
+    <li><button href="login.php" class="nav_btn">Login/Signup</a></li>
+<ul>
+</head>
+<title>Rolsa Technologies</title>
+
+<body>
+    <h1>Home</h1>
+</body>
+</html>
